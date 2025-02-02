1c51b047987e2899b515e849d51c47ac344098ee
ofirgalcon <ofir@gal.co.uk>
2025-02-02 21:07:24 +0000

    Revert "suggestions"
    This reverts commit 8e1bde96a95799ac7c8eb9cc646c031a36fbad3b.

--- views/launchdaemons_tab.php
+++ views/launchdaemons_tab.php
@@ -1,15 +1,4 @@
 <div id="launchdaemons-tab"></div>
-
-<div id="lister" style="font-size: large; float: right;">
-    <a href="/show/listing/launchdaemons/launchdaemons" title="List">
-        <i class="btn btn-default tab-btn fa fa-list"></i>
-    </a>
-</div>
-<div id="report_btn" style="font-size: large; float: right;">
-    <a href="/show/report/launchdaemons/launchdaemons_report" title="Report">
-        <i class="btn btn-default tab-btn fa fa-th"></i>
-    </a>
-</div>
 <h2 data-i18n="launchdaemons.launchdaemons"></h2>
 
 <script>
@@ -18,36 +7,50 @@ $(document).on('appReady', function(){
         // Set count of launchdaemons
         $('#launchdaemons-cnt').text(data.length);
         var skipThese = ['id','serial_number','label'];
-        var $launchdaemonsTab = $('#launchdaemons-tab'); // Cache jQuery selector
+        $.each(data, function(i,d){
 
-        $.each(data, function(i, d){
-            var rows = [];
+            // Generate rows from data
+            var rows = ''
             for (var prop in d){
-                if(skipThese.indexOf(prop) === -1){
-                    if ((d[prop] === '' || d[prop] === null) && d[prop] !== "0"){
-                        continue; // Skip empty values
-                    } else if((prop === "startinterval") && +d[prop] >= 60){
-                        rows.push('<tr><th>'+i18n.t('launchdaemons.'+prop)+'</th><td><span title="'+d[prop]+' '+i18n.t('launchdaemons.seconds')+'">'+moment.duration(+d[prop], "seconds").humanize()+'</span></td></tr>');
-                    } else if((prop === 'disabled' || prop === 'ondemand' || prop === 'runatload' || prop === 'startonmount' || prop === 'keepalive')){
-                        rows.push('<tr><th>'+i18n.t('launchdaemons.'+prop)+'</th><td>'+i18n.t(d[prop] ? 'yes' : 'no')+'</td></tr>');
-                    } else if(prop === 'daemon_json'){
-                        rows.push('<tr><th>'+i18n.t('launchdaemons.'+prop)+'</th><td>'+d[prop].replace(/\n\s+/g, function(match) {
-                            return '<br>' + '&nbsp;'.repeat(match.length - 1);
-                        })+'</td></tr>');
+                // Skip skipThese
+                if(skipThese.indexOf(prop) == -1){
+                    // Do nothing for empty values to blank them
+                    if ((d[prop] == '' || d[prop] == null) && d[prop] != "0"){
+                        rows = rows
+
+                    // Format seconds
+                    } else if((prop == "startinterval") && +d[prop] >= 60){
+                       rows = rows + '<tr><th>'+i18n.t('launchdaemons.'+prop)+'</th><td><span title="'+d[prop]+' '+i18n.t('launchdaemons.seconds')+'">'+moment.duration(+d[prop], "seconds").humanize()+'</span></td></tr>';
+
+                    // Format Yes booleans
+                    } else if((prop == 'disabled' || prop == 'ondemand' || prop == 'runatload' || prop == 'startonmount' || prop == 'keepalive') && d[prop] == 1){
+                       rows = rows + '<tr><th>'+i18n.t('launchdaemons.'+prop)+'</th><td>'+i18n.t('yes')+'</td></tr>';
+                        
+                    // Format No booleans
+                    } else if((prop == 'disabled' || prop == 'ondemand' || prop == 'runatload' || prop == 'startonmount' || prop == 'keepalive') && d[prop] == 0){
+                       rows = rows + '<tr><th>'+i18n.t('launchdaemons.'+prop)+'</th><td>'+i18n.t('no')+'</td></tr>';
+                        
+                    // Format returns
+                    } else if(prop == 'daemon_json' ){
+                       rows = rows + '<tr><th>'+i18n.t('launchdaemons.'+prop)+'</th><td>'+d[prop].replace(/\n      /g,'<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;').replace(/\n     /g,'<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;').replace(/\n    /g,'<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;').replace(/\n   /g,'<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;').replace(/\n  /g,'<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;').replace(/\n /g,'<br>&nbsp;&nbsp;&nbsp;&nbsp;').replace(/\n/g,'<br>')+'</td></tr>';
+                    
+                    // Else, build out rows from items
                     } else {
-                        rows.push('<tr><th>'+i18n.t('launchdaemons.'+prop)+'</th><td>'+d[prop]+'</td></tr>');
+                        rows = rows + '<tr><th>'+i18n.t('launchdaemons.'+prop)+'</th><td>'+d[prop]+'</td></tr>';
                     }
                 }
             }
-            $launchdaemonsTab.append(
-                $('<h4>').append($('<i>').addClass('fa fa-paper-plane')).append(' '+d.label),
-                $('<div>').append(
-                    $('<table>').addClass('table table-striped table-condensed').append(
-                        $('<tbody>').append(rows.join(''))
-                    )
-                )
-            );
-        });
+            $('#launchdaemons-tab')
+                .append($('<h4>')
+                    .append($('<i>')
+                        .addClass('fa fa-paper-plane'))
+                    .append(' '+d.label))
+                .append($('<div>')
+                    .append($('<table>')
+                        .addClass('table table-striped table-condensed')
+                        .append($('<tbody>')
+                            .append(rows))))
+        })
     });
 });
 </script>
